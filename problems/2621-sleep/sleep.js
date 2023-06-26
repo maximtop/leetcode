12a36ee8cbc2d36b842fbd1891509fa6ff9db03e
Maxim Topciu <mtopciu@adguard.com>
2023-06-26 06:41:54 +0300

    add 2621-sleep

---
+++ problems/2621-sleep/sleep.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, millis);
+    });
+}
+
+module.exports = { sleep };
