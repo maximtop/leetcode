b46357da1e40bbfd85199782a991168d7ddc4b21
Maxim Topciu <mtopciu@adguard.com>
2023-09-28 23:11:25 +0300

    2619-array-prototype-last

---
+++ problems/2619-array-prototype-last/last.js
@@ -0,0 +1,15 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+// eslint-disable-next-line no-extend-native
+Array.prototype.last = function () {
+    if (this.length === 0) {
+        return -1;
+    }
+    return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
